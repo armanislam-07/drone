ea0c735fe057bb57f2fc24f20e6e77ae78a3fefa
armanislam-07 <armanislamusa@gmail.com>
2025-07-27 16:49:38 -0700

    build: added the framework for autoleveling with MPU 6050 w/ I2C

---
+++ app/autolevel.c
@@ -0,0 +1,25 @@
+// will start working once the gyro + acceleromter comes in 
+
+
+
+
+
+/*
+if the drone has a tilt banked to the right
+    put either less power (ccr1 ) to the left motors or put more to the righ motor
+
+    vice versa 
+
+think about having higher resolution for pwm over frequency due to fine tunign  ?? 
+
+
+
+
+*/
+
+
+/*
+outline 
+
+
+*/
